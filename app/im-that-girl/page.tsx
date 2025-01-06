ea7414917c12c4dd352f00f821e87429826e3f64
kiranpate1 <kp8568@gmail.com>
2025-01-05 21:20:18 -0500

    minor updates

--- app/im-that-girl/page.tsx
+++ app/im-that-girl/page.tsx
@@ -15,13 +15,11 @@ declare global {
 
 const Cozy: React.FC = () => {
   useEffect(() => {
-    const container = document.querySelector(
-      ".scroll-container"
-    ) as HTMLElement;
+    const container = document.querySelector("main") as HTMLElement;
     let containerHalf = container.getBoundingClientRect().width / 4;
 
     const containerItems = document.querySelectorAll(
-      ".scroll-container > div"
+      ".scroll-items > div"
     ) as NodeListOf<HTMLElement>;
     const selectors = document.querySelector(".selectors") as HTMLElement;
     const h1 = document.querySelector("h1") as HTMLElement;
@@ -31,6 +29,17 @@ const Cozy: React.FC = () => {
       selector.classList.add("selector");
       selectors.appendChild(selector);
     });
+    const selectorsAll = document.querySelectorAll(
+      ".selector"
+    ) as NodeListOf<HTMLElement>;
+    let containerItemsLeft: number[] = [];
+
+    for (let i = 0; i < containerItems.length; i++) {
+      var itemLeft = containerItems[i].getBoundingClientRect().left;
+      var itemCenter = itemLeft - containerHalf;
+
+      containerItemsLeft.push(itemCenter);
+    }
 
     function setup() {
       const phrases = [
@@ -87,36 +96,131 @@ const Cozy: React.FC = () => {
     handleAnimateIntroScroll.throttled = false;
 
     function animateIntro() {
-      // section1();
-      // function section1() {
-      //   var container = document.querySelector(".intro") as HTMLElement;
-      //   var containerHeight = container.getBoundingClientRect().height;
-      //   const path =
-      //     container.getBoundingClientRect().top - containerHeight * 0.1;
-      //   const limit = containerHeight * 0.6;
-      //   const progress = Math.max(0, Math.min(-path / limit, 1));
-      // }
-      // section2();
-      // function section2() {
-      //   var container = document.querySelector(".no1") as HTMLElement;
-      //   var containerHeight = container.getBoundingClientRect().height;
-      //   //const offset = window.innerHeight * 0.4;
-      //   const offset = 0;
-      //   const path = container.getBoundingClientRect().top - offset;
-      //   const end = containerHeight - offset;
-      //   if (path >= 0) {
-      //   }
-      //   if (path < 0 && path > -end) {
-      //     var progress = path / -end;
-      //     var ease = easeInQuad(progress);
-      //   }
-      //   if (path <= -end) {
-      //   }
-      // }
+      section1();
+      function section1() {
+        // var container = document.querySelector(".no1") as HTMLElement;
+        // var containerHeight = container.getBoundingClientRect().height;
+        // //const offset = window.innerHeight * 0.4;
+        // const offset = 0;
+        // const path = container.getBoundingClientRect().top - offset;
+        // const end = containerHeight - offset;
+        // if (path >= 0) {
+        // }
+        // if (path < 0 && path > -end) {
+        //   var progress = path / -end;
+        //   var ease = easeInQuad(progress);
+        // }
+        // if (path <= -end) {
+        // }
+      }
+
+      section2();
+      function section2() {
+        const scroll = document.querySelector("#scroll-section") as HTMLElement;
+        const scrollContainer = document.querySelector(
+          "#scroll-container"
+        ) as HTMLElement;
+        const scrollHeight = scroll.getBoundingClientRect().height;
+        const scrollTop = window.scrollY;
+        const scrollStart = scroll.offsetTop - window.innerHeight;
+        const scrollEnd = scrollStart + scrollHeight - window.innerHeight;
+        const scrollProgress =
+          Math.max(
+            0,
+            Math.min((scrollTop - scrollStart) / (scrollEnd - scrollStart), 1)
+          ) * 5600;
+
+        scrollContainer.scrollLeft = scrollProgress - containerHalf;
+
+        containerItems.forEach((item, i) => {
+          const img = item.querySelector("img") as HTMLImageElement;
+          const start = containerItemsLeft[i] + 100;
+          const end = start + 400; //width of item goes here
+          const approaching = start - 700; //change on resize
+          const leaving = end + 700; //change on resize
+          const translateZ = 400;
+          const rotateY = 30 * 2;
+          const blur = 20;
+
+          //before
+          if (scrollProgress < approaching) {
+            img.style.transform = `translateX(100%) translateZ(${-translateZ}px) rotateY(0deg)`;
+            img.style.filter = `blur(${blur}px)`;
+            selectorsAll[i].style.opacity = "0.25";
+          }
+          //approaching
+          if (scrollProgress > approaching && scrollProgress <= start) {
+            var progress = Math.min(
+              (scrollProgress - approaching) / (start - approaching),
+              1
+            );
+            var ease = easeInOutQuint(progress);
+            if (progress < 0.5) {
+              img.style.transformOrigin = `${100 - ease * 100}% center`;
+              img.style.transform = `translateX(${
+                100 - ease * 50
+              }%) translateZ(${-translateZ + ease * translateZ}px) rotateY(${
+                progress * rotateY
+              }deg)`;
+              img.style.filter = `blur(${blur - ease * blur}px)`;
+            } else if (progress > 0.5) {
+              img.style.transformOrigin = `${100 - ease * 100}% center`;
+              img.style.transform = `translateX(${
+                100 - ease * 50
+              }%) translateZ(${-translateZ + ease * translateZ}px) rotateY(${
+                rotateY - progress * rotateY
+              }deg)`;
+              img.style.filter = `blur(${blur - ease * blur}px)`;
+            }
+            selectorsAll[i].style.opacity = "0.25";
+          }
+          //inside
+          if (scrollProgress > start && scrollProgress < end) {
+            img.style.transform =
+              "translateX(50%) translateZ(0px) rotateY(0deg)";
+            img.style.filter = "blur(0px)";
+            selectorsAll[i].style.opacity = "1";
+            h1.innerHTML = img.alt;
+          }
+          //leaving
+          if (scrollProgress >= end && scrollProgress < leaving) {
+            var progress = Math.min(
+              (scrollProgress - end) / (leaving - end),
+              1
+            );
+            var ease = easeInOutQuint(progress);
+            if (progress < 0.5) {
+              img.style.transformOrigin = `${100 - ease * 100}% center`;
+              img.style.transform = `translateX(${
+                50 - ease * 50
+              }%) translateZ(${0 - ease * translateZ}px) rotateY(${
+                -progress * rotateY
+              }deg)`;
+              img.style.filter = `blur(${ease * blur}px)`;
+            } else if (progress > 0.5) {
+              img.style.transformOrigin = `${100 - ease * 100}% center`;
+              img.style.transform = `translateX(${
+                50 - ease * 50
+              }%) translateZ(${0 - ease * translateZ}px) rotateY(${
+                -rotateY + progress * rotateY
+              }deg)`;
+              img.style.filter = `blur(${ease * blur}px)`;
+            }
+            selectorsAll[i].style.opacity = "0.25";
+          }
+          //after
+          if (scrollProgress > leaving) {
+            img.style.transform = `translateX(0%) translateZ(${-translateZ}px) rotateY(0deg)`;
+            img.style.filter = `blur(${blur}px)`;
+            selectorsAll[i].style.opacity = "0.25";
+          }
+        });
+      }
     }
 
     //easing
-    function easeTiles(t: number) {
+
+    function easeInOutQuint(t: number) {
       t /= 0.5;
       if (t < 1) return 0.5 * Math.pow(t, 5);
       t -= 2;
@@ -134,110 +238,6 @@ const Cozy: React.FC = () => {
     window.addEventListener("resize", handleAnimateIntroResize, {
       passive: true,
     });
-
-    const selectorsAll = document.querySelectorAll(
-      ".selector"
-    ) as NodeListOf<HTMLElement>;
-    let containerItemsLeft: number[] = [];
-
-    for (let i = 0; i < containerItems.length; i++) {
-      var itemLeft = containerItems[i].getBoundingClientRect().left;
-      var itemCenter = itemLeft - containerHalf;
-
-      containerItemsLeft.push(itemCenter);
-    }
-
-    window.addEventListener("wheel", function (event) {
-      event.preventDefault();
-      container.scrollLeft += event.deltaY;
-
-      animate();
-    });
-
-    function animate() {
-      const scrollProgress = container.scrollLeft + containerHalf;
-
-      containerItems.forEach((item, i) => {
-        const img = item.querySelector("img") as HTMLImageElement;
-        const start = containerItemsLeft[i] + 100;
-        const end = start + 400; //width of item goes here
-        const approaching = start - 700; //change on resize
-        const leaving = end + 700; //change on resize
-        const translateZ = 400;
-        const rotateY = 30 * 2;
-        const blur = 20;
-
-        //before
-        if (scrollProgress < approaching) {
-          img.style.transform = `translateX(100%) translateZ(${-translateZ}px) rotateY(0deg)`;
-          img.style.filter = `blur(${blur}px)`;
-          selectorsAll[i].style.opacity = "0.25";
-        }
-        //approaching
-        if (scrollProgress > approaching && scrollProgress <= start) {
-          var progress = Math.min(
-            (scrollProgress - approaching) / (start - approaching),
-            1
-          );
-          var ease = easeInOutQuint(progress);
-          if (progress < 0.5) {
-            img.style.transformOrigin = `${100 - ease * 100}% center`;
-            img.style.transform = `translateX(${100 - ease * 50}%) translateZ(${
-              -translateZ + ease * translateZ
-            }px) rotateY(${progress * rotateY}deg)`;
-            img.style.filter = `blur(${blur - ease * blur}px)`;
-          } else if (progress > 0.5) {
-            img.style.transformOrigin = `${100 - ease * 100}% center`;
-            img.style.transform = `translateX(${100 - ease * 50}%) translateZ(${
-              -translateZ + ease * translateZ
-            }px) rotateY(${rotateY - progress * rotateY}deg)`;
-            img.style.filter = `blur(${blur - ease * blur}px)`;
-          }
-          selectorsAll[i].style.opacity = "0.25";
-        }
-        //inside
-        if (scrollProgress > start && scrollProgress < end) {
-          img.style.transform = "translateX(50%) translateZ(0px) rotateY(0deg)";
-          img.style.filter = "blur(0px)";
-          selectorsAll[i].style.opacity = "1";
-          h1.innerHTML = img.alt;
-        }
-        //leaving
-        if (scrollProgress >= end && scrollProgress < leaving) {
-          var progress = Math.min((scrollProgress - end) / (leaving - end), 1);
-          var ease = easeInOutQuint(progress);
-          if (progress < 0.5) {
-            img.style.transformOrigin = `${100 - ease * 100}% center`;
-            img.style.transform = `translateX(${50 - ease * 50}%) translateZ(${
-              0 - ease * translateZ
-            }px) rotateY(${-progress * rotateY}deg)`;
-            img.style.filter = `blur(${ease * blur}px)`;
-          } else if (progress > 0.5) {
-            img.style.transformOrigin = `${100 - ease * 100}% center`;
-            img.style.transform = `translateX(${50 - ease * 50}%) translateZ(${
-              0 - ease * translateZ
-            }px) rotateY(${-rotateY + progress * rotateY}deg)`;
-            img.style.filter = `blur(${ease * blur}px)`;
-          }
-          selectorsAll[i].style.opacity = "0.25";
-        }
-        //after
-        if (scrollProgress > leaving) {
-          img.style.transform = `translateX(0%) translateZ(${-translateZ}px) rotateY(0deg)`;
-          img.style.filter = `blur(${blur}px)`;
-          selectorsAll[i].style.opacity = "0.25";
-        }
-      });
-
-      function easeInOutQuint(t: number) {
-        t /= 0.5;
-        if (t < 1) return 0.5 * Math.pow(t, 5);
-        t -= 2;
-        return 0.5 * (Math.pow(t, 5) + 2);
-      }
-    }
-
-    animate();
   }, []);
   return (
     <main>
@@ -247,11 +247,14 @@ const Cozy: React.FC = () => {
           className="h-screen w-screen flex flex-col justify-end items-center text-white"
         ></div>
       </div>
-      <section className="h-[400vh]">
-        <div className="h-screen w-screen relative text-white"></div>
-        <div className="sticky top-0 left-0 w-screen h-screen overflow-hidden">
+      <section className="h-screen w-screen relative text-white"></section>
+      <section id="scroll-section" className="h-[400vh]">
+        <div
+          id="scroll-container"
+          className="sticky top-0 left-0 w-screen h-screen overflow-hidden"
+        >
           <div className="selectors"></div>
-          <div className="scroll-container">
+          <div className="scroll-items absolute h-full flex items-center overflow-scroll py-0 px-[50vw]">
             <div>
               <img
                 src="https://assets.vogue.com/photos/64efa2113e89acbbb62489f4/master/w_1600%2Cc_limit/beyonce-marni-parkwood.jpg"
